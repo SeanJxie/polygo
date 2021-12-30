f6ec75fa0d981ff56cc8afebd5b0be8b2c965eab
Sean Xie <47431127+SeanJxie@users.noreply.github.com>
2021-12-30 17:51:54 -0500

    Update polynomial_test.go

--- polynomial_test.go
+++ polynomial_test.go
@@ -10,9 +10,9 @@ func TestQuadCoefficients(t *testing.T) {
 	quad, _ := NewRealPolynomial(quadCoefficients)
 	root, _ := quad.FindRootWithin(-1, 1)
 
-	quadraticExpression := quad.Expr()
-	if quadraticExpression[:len(quadraticExpression)-1] != "0.000000x^0 + 0.000000x^1 + 2.000000x^2" {
-		t.Fatalf("error in calculation -'%v'", quadraticExpression[:len(quadraticExpression)-1])
+	quadraticExpression := quad.String()
+	if quadraticExpression != "0.000000x^0 + 0.000000x^1 + 2.000000x^2" {
+		t.Fatalf("error in calculation -'%v'", quadraticExpression)
 	}
 
 	if root != 0.0 {
